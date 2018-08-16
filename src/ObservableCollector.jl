60a1d40e6acb38afb3e288ce811ca439789909a4
Stephan Kleinboelting <skleinbo@thp.uni-koeln.de>
2018-08-16 08:43:03 +0200

    I begin to understand macros better.
    Fixed a bug that didn't allow passing variables to the macros. Basically 
    rewrote them all.

--- src/ObservableCollector.jl
+++ src/ObservableCollector.jl
@@ -2,62 +2,72 @@ module ObservableCollector
 
 export  @at,
         @every,
+        @condition,
         @observations
 
-function get_arguments(args)
-    name, map, step = ("", nothing, 0)
-    for arg in args
-        if typeof(arg)==String
-            name = arg
-        elseif typeof(arg)<:Integer
-            step = arg
-        else
-            map = arg
-        end
-    end
-    if name == "" || map == nothing
-        error("Invalid arguments to @at")
-    end
-    (name,map,step)
-end
 
 macro at(args...)
     if length(args)!==3
         throw(ArgumentError("@at requires 3 arguments."))
     end
-    name,map,step = get_arguments(args)
-    return esc(:( (name=$name, map=$map, condition=(state,t)->t==$step) ))
+    esc(quote
+        eval_args = eval.($args)
+        @assert reduce(&, (<:).(typeof.(eval_args), (Integer,String,Function))) == true "Wrong argument types to @at: $eval_args."
+        local step,name,map = eval_args
+        name = Symbol(name)
+        if step == 0
+            throw(ArgumentError("Step length cannot be zero."))
+        end
+        return (name=name, map=map, condition=(state,t)->(t == step)==0)
+    end)
 end
 
 macro every(args...)
     if length(args)!==3
         throw(ArgumentError("@every requires 3 arguments."))
     end
-    name,map,step = get_arguments(args)
-    return esc(:( (name=$name, map=$map, condition=(state,t)->(t% $step)==0) ))
+    esc(quote
+        eval_args = eval.($args)
+        @assert reduce(&, (<:).(typeof.(eval_args), (Integer,String,Function))) == true "Wrong argument types to @every: $eval_args."
+        local step,name,map = eval_args
+        name = Symbol(name)
+        if step == 0
+            throw(ArgumentError("Step length cannot be zero."))
+        end
+        return (name=name, map=map, condition=(state,t)->(t % step)==0)
+    end)
 end
 
+macro condition(args...)
+    if length(args)!==3
+        throw(ArgumentError("@condition requires 3 arguments."))
+    end
+    esc(quote
+        eval_args = eval.($args)
+        @assert reduce(&, (<:).(typeof.(eval_args), (Function,String,Function))) == true "Wrong argument types to @condition: $eval_args."
+        local condition,name,map = eval_args
+        name = Symbol(name)
+        return (name=name, map=map, condition=condition)
+    end)
+end
 
 macro observations(ex::Expr)
-    bodyparts = Expr[]
-    for subexpr in ex.args
-        if typeof(subexpr)==Expr && subexpr.head==:macrocall
-            f = eval(subexpr)
-            bodypart = quote
-                if $(f.condition)(state,t)
-                    push!(output, ($(f.name),$(f.map)(state,t)))
+    bodyparts = filter(x->typeof(x)==Expr && x.head==:macrocall,ex.args)
+
+    esc(quote
+        clauses = eval.($bodyparts)
+        global func = :(;)
+        for clause in clauses
+            global func = quote
+                $func;
+                if (($clause).condition)(state,t)
+                    push!(output, (name=($clause).name,val=(($clause).map)(state,t)))
                 end
             end
-            push!(bodyparts, bodypart)
         end
-    end
-    fullbody = :()
-    for bodypart in bodyparts
-        fullbody = :($fullbody; $bodypart)
-    end
-    return quote
-        (output, state, t) -> $fullbody;
-    end
+        eval( :( (output, state, t) -> begin $func; nothing end ) )
+    end)
+
 end
 
 end
--- test/ObservationsTests.jl
+++ test/ObservationsTests.jl
@@ -6,15 +6,25 @@ output = []
 obs01 = @observations begin
 end
 obs02 = @observations begin
-    @every 2 "O1" (s,t)->t
+    @every 2 "O2" (s,t)->t
+end
+obs03 = @observations begin
+    @condition (s,t)->s+t==5 "sumtofive" (s,t)->t
 end
 
-@test obs01(output,0,0) == ()
 
 @test begin
     output = []
     for t in 1:10
         obs02(output,0,t)
     end
-    output == [("O1",2),("O1",4),("O1",6),("O1",8),("O1",10)]
+    output == [(name=:O2, val=2j) for j in 1:5]
+end
+
+@test begin
+    output = []
+    for t in 1:10
+        obs03(output,1,t)
+    end
+    output == [(name=:sumtofive, val=4)]
 end
