08f7e2a9c8e854fa99547855cc0dd104b07afb73
Stephan Kleinboelting <skleinbo@thp.uni-koeln.de>
2018-08-19 10:55:22 +0200

    docstrings

--- src/ObservableCollector.jl
+++ src/ObservableCollector.jl
@@ -5,6 +5,20 @@ export  @at,
         @condition,
         @observations
 
+"""
+    @condition(cond, block)
+
+Define multiple observables to be evaluated under the same condition. Use `-->`
+to define pairs of `name --> map`.
+
+# Example
+```julia
+@condition (s,t)->t>=3 begin
+    "S" --> (s,t)->s^2
+    "T" --> (s,t)->t
+end
+```
+"""
 macro condition(condition, block)
     typeof(block)==Expr && block.head == :block || throw(ArgumentError("b in @condition(a, b) must be a block."))
     subexpr = filter(x->(typeof(x)==Expr && x.head==:-->), block.args)
@@ -21,27 +35,76 @@ macro condition(condition, block)
     end)
 end
 
-macro condition(args...)
-    if length(args)!==3
-        throw(ArgumentError("@condition requires 3 arguments."))
-    end
+"""
+    @condition(cond, name, map)
+
+Creates a clause to collect an observable `map(state,time)` called `name` under
+condition `cond(state,time)`.
+
+See also: [`@at`](@ref), [`@every`](@ref)
+"""
+macro condition(cond, name, map)
     esc(quote
-        @condition $(args[1]) begin $(args[2]) --> $(args[3]) end
+        @condition $cond begin $name --> $map end
     end)
 end
 
+"""
+    @at(N::Integer, name, map)
+
+Creates a clause to collect an observable `map(state,time)` called `name` at
+timestep `N`.
+
+See also: [`@condition`](@ref)
+"""
 macro at(args...)
     esc(quote
         @condition (s,t)->t==$(args[1]) $(args[2]) $(args[3])
     end)
 end
 
+"""
+    @every(N::Integer, name, map)
+
+Creates a clause to collect an observable `map(state,time)` called `name` every
+`N` timesteps, i.e `t%N==0`.
+
+See also: [`@condition`](@ref)
+"""
 macro every(args...)
     esc(quote
         @condition (s,t)->t % $(args[1])==0 $(args[2]) $(args[3])
     end)
 end
 
+"""
+    @observations(block)
+
+Creates an anonymous function with arguments `(output::Vector{Any}, state, time)`
+    that collects observables under conditions defined in `block`.
+
+# Example:
+```julia
+output = []
+obs = @observations begin
+    @condition (s,t)->t>=3 begin
+        "A" --> (s,t)->s
+        "T" --> (s,t)->t
+    end
+end
+for t in 1:4
+    obs(output,1,t)
+end
+
+julia> @show output
+output = Any[(name = :A, val = 1), (name = :T, val = 3), (name = :A, val = 1), (name = :T, val = 4)]
+4-element Array{Any,1}:
+ (name = :A, val = 1)
+ (name = :T, val = 3)
+ (name = :A, val = 1)
+ (name = :T, val = 4)
+```
+"""
 macro observations(ex::Expr)
     bodyparts = filter(x->typeof(x)==Expr && x.head==:macrocall,ex.args)
 
@@ -67,7 +130,6 @@ macro observations(ex::Expr)
                     $inner;
                 end
             end
-
         end
         eval( :( (output, state, t) -> begin $func; nothing end ) )
     end)
